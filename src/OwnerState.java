97f6b84f679bd8445165366f08c10ad50a670360
cyrilletcc <cyrilleteupe@gmail.com>
2022-03-24 18:13:28 -0400

    Update OwnerState.java

--- src/OwnerState.java
+++ src/OwnerState.java
@@ -6,8 +6,38 @@
 
 /**
  *
- * @author cyril
+ * @author Cyrille
  */
+
+
 public class OwnerState {
+    //Overview: OwnerState is immutable singleton class
+    //
+    //The abstraction function is:
+    //
+    // The rep invarian is: 
+    // The rep
+    private static OwnerState s = null;
+    private String username = "admin";
+    private String password = "admin";
+    
+    private OwnerState(){
+        
+    }
+    public static OwnerState getInstance(){
+        if (s == null){
+            s = new OwnerState();
+        }
+        return s;
+    }
+    
+    public void logout(){
+        
+    }
+    public void addBook(Book book){}
+    public void removeBook(Book book){}
+    public void registerNewCustomer(String name, String password){}
+    public void deleteCustomer(String name, String password){}
+    
     
 }
