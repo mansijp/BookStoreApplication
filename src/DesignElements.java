6cfb60b58aed4e0334fbddcf002db3bf51a82ebd
Mansi Patel <45537538+mansijp@users.noreply.github.com>
2022-03-31 14:54:47 -0400

    Merge branch 'main' of https://github.com/Fungeey/BookStoreApplication
    # Conflicts:
    #	src/Store.java

---
+++ src/DesignElements.java
@@ -0,0 +1,33 @@
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mansi
+ */
+public class DesignElements {
+    
+    public void masterBtn(Button btn){
+        Font font = new Font.font("Verdana",FontWeight.BOLD, 20);
+
+    }
+    
+    public void masterLabel(Label label){
+        Font font = new Font("Vardane", 26);
+        label.setFont(font);
+    } 
+    
+    
+}
